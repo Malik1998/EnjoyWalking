17b11fe9b98ab8197ef09f4832466cab0aba26b8
gazizullin <gazizullin.malik2010@yandex.ru>
2018-12-01 12:46:00 +0300

    D

--- EnjoyWalking/start_page/views.py
+++ EnjoyWalking/start_page/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.shortcuts import render
 
 from django.shortcuts import render
 
+
 # Create your views here.
+def post_list(request):
+    return render(request, 'start_page/index.html', {})
