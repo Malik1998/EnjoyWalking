e31aa4c8f97450b9f244bb850a8d4a89bd79c1e5
ujnomw <m.golovatskih@gmail.com>
2018-12-01 14:03:26 +0300

    comment changed

--- EnjoyWalking/start_page/models.py
+++ EnjoyWalking/start_page/models.py
@@ -7,6 +7,6 @@ from django.db import models
 # Create your models here.
 class Point(models.Model):
     latitude = models.CharField(max_length=100) # shirota
-    longtitude = models.CharField(max_length=100)# dolgota
+    longtitude = models.CharField(max_length=100) # dolgota
     rating = models.FloatField()
     description = models.TextField()
