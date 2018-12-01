0adc5b8134ff769b0192a4fce48d6cfc4c78d6f7
gazizullin <gazizullin.malik2010@yandex.ru>
2018-12-01 14:58:21 +0300

    add

--- EnjoyWalking/start_page/models.py
+++ EnjoyWalking/start_page/models.py
@@ -3,10 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
-class Point:
-    latitude = models.Charfield() # shirota
-    longtitude = models.Charfield() # dolgota
+class Point(models.Model):
+    latitude = models.CharField(max_length=100) # shirota
+    longtitude = models.CharField(max_length=100)# dolgota
     rating = models.FloatField()
     description = models.TextField()
-    
